80baf41cb68e01802bb52b810562cb6f0ba25a39
haritestaccount <hariclashofclans7@gmail.com>
2025-04-29 00:54:31 +0530

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -1,31 +1,43 @@
 package commands
 
 import (
-	"EverythingSuckz/fsb/config"
-	"EverythingSuckz/fsb/internal/utils"
+    "EverythingSuckz/fsb/config"
+    "EverythingSuckz/fsb/internal/utils"
 
-	"github.com/celestix/gotgproto/dispatcher"
-	"github.com/celestix/gotgproto/dispatcher/handlers"
-	"github.com/celestix/gotgproto/ext"
-	"github.com/celestix/gotgproto/storage"
+    "github.com/celestix/gotgproto/dispatcher"
+    "github.com/celestix/gotgproto/dispatcher/handlers"
+    "github.com/celestix/gotgproto/ext"
+    "github.com/celestix/gotgproto/storage"
 )
 
 func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
-	log := m.log.Named("start")
-	defer log.Sugar().Info("Loaded")
-	dispatcher.AddHandler(handlers.NewCommand("start", start))
+    log := m.log.Named("start")
+    defer log.Sugar().Info("Loaded")
+    dispatcher.AddHandler(handlers.NewCommand("start", start))
 }
 
 func start(ctx *ext.Context, u *ext.Update) error {
-	chatId := u.EffectiveChat().GetID()
-	peerChatId := ctx.PeerStorage.GetPeerById(chatId)
-	if peerChatId.Type != int(storage.TypeUser) {
-		return dispatcher.EndGroups
-	}
-	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
-		ctx.Reply(u, "You are not allowed to use this bot.", nil)
-		return dispatcher.EndGroups
-	}
-	ctx.Reply(u, "Need a direct streamable link to a file? Send it my way! 🤓\n\nJoin my Update Channel @haris_garage 🗿 for more updates.\n\nLink validity: 24 hours ⏳\n\nPro Tip: Use 1DM Browser for lightning-fast downloads! 🔥", nil)
-	return dispatcher.EndGroups
+    chatId := u.EffectiveChat().GetID()
+    peerChatId := ctx.PeerStorage.GetPeerById(chatId)
+    if peerChatId.Type != int(storage.TypeUser) {
+        return dispatcher.EndGroups
+    }
+    if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
+        ctx.Reply(u, "You are not allowed to use this bot.", nil)
+        return dispatcher.EndGroups
+    }
+
+    // Force subscribe check
+    channelUsername := "@haris_garage" // Replace with your channel username
+    chatMember, err := ctx.Bot.GetChatMember(channelUsername, chatId)
+    if err != nil || chatMember.Status == "left" || chatMember.Status == "kicked" {
+        // User is not subscribed to the required channel
+        ctx.Reply(u, "Please join my channel "+channelUsername+" to use this bot.", nil)
+        ctx.Reply(u, "Join here: https://t.me/haris_garage, nil)
+        return dispatcher.EndGroups // End further processing
+    }
+
+    // Proceed with normal functionality if subscribed
+    ctx.Reply(u, "Need a direct streamable link to a file? Send it my way! 🤓\n\nJoin my Update Channel "+channelUsername+" 🗿 for more updates.\n\nLink validity: 24 hours ⏳\n\nPro Tip: Use 1DM Browser for lightning-fast downloads! 🔥", nil)
+    return dispatcher.EndGroups
 }
